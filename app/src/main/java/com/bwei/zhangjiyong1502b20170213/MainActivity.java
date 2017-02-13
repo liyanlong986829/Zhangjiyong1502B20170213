8811f505b3ce82cdf47029cf75cf08c87003ea99
liyanlong986829 <526970264@qq.com>
2017-02-13 18:34:25 +0800

    Initial commit

---
+++ app/src/main/java/com/bwei/zhangjiyong1502b20170213/MainActivity.java
@@ -0,0 +1,17 @@
+package com.bwei.zhangjiyong1502b20170213;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 自定义画圆和TextView，圆中心写显示随机数(TextView)
+ * TextView需要自定义属性
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
